e885745b8e64162191ab557fca838957cddd185a
Anthony Whitford <anthony@whitford.com>
2011-09-05 09:45:52 -0700

    Added use case demonstrating bug recorded as Issue 269.

--- test-maven-lombok/src/main/lombok/org/projectlombok/test/AnotherDataExample.java
+++ test-maven-lombok/src/main/lombok/org/projectlombok/test/AnotherDataExample.java
@@ -1,7 +1,12 @@
 package org.projectlombok.test;
 
 import lombok.Data;
+// http://code.google.com/p/projectlombok/issues/detail?id=146
 //import lombok.NonNull;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +23,22 @@ public class AnotherDataExample implements Useful {
 
     public void doSomething() {
         LOGGER.debug("Doing something useful...");
+    }
+
+    // Example related to http://code.google.com/p/projectlombok/issues/detail?id=269
+    @XmlType
+    @XmlEnum(Integer.class)
+    public enum Coin { 
+        @XmlEnumValue("1") PENNY(1),
+        @XmlEnumValue("5") NICKEL(5),
+        @XmlEnumValue("10") DIME(10),
+        @XmlEnumValue("25") QUARTER(25);
+
+        private final int cents;
+
+        Coin (final int cents) {
+            this.cents = cents;
+        }
     }
 }
 
